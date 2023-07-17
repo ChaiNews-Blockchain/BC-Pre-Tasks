7e4737877d02bd761f02f6c7d6735a1316423100
Yuşa Eren Gündoğdu <77022291+YusaErenGnd@users.noreply.github.com>
2023-07-17 16:59:11 +0300

    Add files via upload

---
+++ Tasks/task_2.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+contract Management {
+    address public manager;
+    uint256 public monthlyFee;
+    
+    struct Payment {
+        uint256 amount;
+        uint256 timestamp;
+    }
+
+    mapping(address => Payment[]) public payments;
+    
+    event PaymentMade(address indexed payer, uint256 amount, uint256 timestamp);
+    event Withdrawal(address indexed manager, uint256 amount, uint256 timestamp);
+
+    modifier onlyManager() {
+        require(msg.sender == manager, "You are not an authorized user.");
+        _;
+    }
+    
+    constructor(uint256 _monthlyFee) {
+        manager = msg.sender;
+        monthlyFee = _monthlyFee;
+    }
+    
+    function makePayment() external payable {
+        require(msg.value == monthlyFee, "Incorrect payment amount.");
+        
+        Payment memory newPayment = Payment({
+            amount: msg.value,
+            timestamp: block.timestamp
+        });
+        payments[msg.sender].push(newPayment);
+        
+        emit PaymentMade(msg.sender, msg.value, block.timestamp);
+    }
+    
+    function getLastPaymentTimestamp(address payer) external view returns (uint256) {
+        uint256 paymentCount = payments[payer].length;
+        require(paymentCount > 0, "No payments found for the given address.");
+        
+        return payments[payer][paymentCount - 1].timestamp;
+    }
+    
+    function withdrawFunds(uint256 amount) external onlyManager {
+        require(amount <= address(this).balance, "Insufficient contract balance.");
+        
+        payable(manager).transfer(amount);
+        
+        emit Withdrawal(manager, amount, block.timestamp);
+    }
+}
+// Event ve emit kodlarını bilmiyordum. Araştırarak yaptım hata olabilir.
