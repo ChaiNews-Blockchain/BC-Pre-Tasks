0e17a7cad529932c54f02b3dd2b1382e74608b31
Muhammed Ekici <ekcmuhammed08@gmail.com>
2023-09-21 16:48:39 +0300

    gitignore updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config()
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const account_1 = process.env.PRIVATE_KEY_1;
+const account_2 = process.env.PRIVATE_KEY_2;
 
 
 module.exports = {
@@ -10,11 +11,11 @@ module.exports = {
     networks: {
       sepolia: {
         url: `https://eth-sepolia.g.alchemy.com/v2/vcbcl5J3wd1EAIpXEVGLzuhZ8EiSsghi`,
-        accounts: [`138806ecadc3cc6396565bdf585d832163441f9107ba7ecc50b997ed8889c42c`,`922a22ce57256389552299b8f811acca70fab425103106e883f4bbe7ed0b94fb`]
+        accounts: [account_1,account_2]
       },
       goerli: {
         url: `https://eth-goerli.g.alchemy.com/v2/3Gd7HXZEQILrdB7yMKzj0rVZSKE1yvBS`,
-        accounts: [`138806ecadc3cc6396565bdf585d832163441f9107ba7ecc50b997ed8889c42c`,`922a22ce57256389552299b8f811acca70fab425103106e883f4bbe7ed0b94fb`],
+        accounts: [account_1,account_2],
         gas: 10000000
       }
     },
